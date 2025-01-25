c630698291aabfbefce03119f489de90ca79b165
Daniela Castro <d.tcast@gmail.com>
2025-01-25 13:11:53 -0300

    Migration con tablas, se agrega ciudad y región con conexión a BD Postgress

--- app_reporte/models.py
+++ app_reporte/models.py
@@ -10,8 +10,22 @@ class PlanPPDA(models.Model):
     def __str__(self):
         return self.nombre
 
+class Region(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=255)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="ciudades")
+
+    def __str__(self):
+        return self.nombre
+
 class Comuna(models.Model):
     nombre = models.CharField(max_length=255)
+    ciudad = models.ForeignKey('Ciudad', on_delete=models.CASCADE, related_name='comunas', default=1) 
 
     def __str__(self):
         return self.nombre
