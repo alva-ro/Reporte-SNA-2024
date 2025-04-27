9617a52ed522c385c21cea4b88f47dba41262ebf
Alvaro <alvaro.t.pardo@gmail.com>
2025-04-27 18:19:38 -0400

    Inclusión CI Set de pruebas minimas para testing de CI

--- app_reporte/tests.py
+++ app_reporte/tests.py
@@ -1,3 +1,97 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from app_reporte.models import Region, Ciudad, Comuna
 # Create your tests here.
+
+class LocalidadTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.region = Region.objects.create(
+            nombre="Región de Valparaiso"
+        )
+        self.ciudad = Ciudad.objects.create(
+            nombre="ConCon",
+            region=self.region
+        )
+        self.comuna = Comuna.objects.create(
+            nombre="ConCon",
+            ciudad=self.ciudad
+        )
+
+    def test_get_regiones(self):
+        respuesta = self.client.get('/api/regiones/')
+        self.assertEqual(respuesta.status_code, 200)
+        self.assertJSONEqual(respuesta.content, [
+            {
+                "nombre": "Región de Valparaiso",
+                "id": self.region.id
+            }
+        ])
+
+    def test_get_ciudades(self):
+        respuesta = self.client.get('/api/ciudades/')
+        self.assertEqual(respuesta.status_code, 200)
+        self.assertJSONEqual(respuesta.content, [
+            {
+                "nombre": "ConCon",
+                "region": self.region.id,
+                "id": self.ciudad.id
+            }
+        ])
+
+    def test_get_comunas(self):
+        respuesta = self.client.get('/api/comunas/')
+        self.assertEqual(respuesta.status_code, 200)
+        self.assertJSONEqual(respuesta.content, [
+            {
+                "nombre": "ConCon",
+                "ciudad": self.ciudad.id,
+                "id": self.comuna.id
+            }
+        ])
+
+class LocalidadTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.region = Region.objects.create(
+            nombre="Región de Valparaiso"
+        )
+        self.ciudad = Ciudad.objects.create(
+            nombre="ConCon",
+            region=self.region
+        )
+        self.comuna = Comuna.objects.create(
+            nombre="ConCon",
+            ciudad=self.ciudad
+        )
+
+    def test_get_regiones(self):
+        respuesta = self.client.get('/api/regiones/')
+        self.assertEqual(respuesta.status_code, 200)
+        self.assertJSONEqual(respuesta.content, [
+            {
+                "nombre": "Región de Valparaiso",
+                "id": self.region.id
+            }
+        ])
+
+    def test_get_ciudades(self):
+        respuesta = self.client.get('/api/ciudades/')
+        self.assertEqual(respuesta.status_code, 200)
+        self.assertJSONEqual(respuesta.content, [
+            {
+                "nombre": "ConCon",
+                "region": self.region.id,
+                "id": self.ciudad.id
+            }
+        ])
+
+    def test_get_comunas(self):
+        respuesta = self.client.get('/api/comunas/')
+        self.assertEqual(respuesta.status_code, 200)
+        self.assertJSONEqual(respuesta.content, [
+            {
+                "nombre": "ConCon",
+                "ciudad": self.ciudad.id,
+                "id": self.comuna.id
+            }
+        ])
